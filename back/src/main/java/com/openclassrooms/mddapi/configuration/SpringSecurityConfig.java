b749cc0cafa98d891c09618a16d4e0d47391da27
Eric Heintzmann <heintzmann.eric@free.fr>
2024-04-17 18:06:52 +0200

    back: replace with lambda

--- back/src/main/java/com/openclassrooms/mddapi/configuration/SpringSecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/SpringSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -53,15 +52,12 @@ public class SpringSecurityConfig {
      */
     @Bean
     public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                User user = userRepository.findByEmail(username)
-                        .orElseThrow(
-                                () -> new UsernameNotFoundException("User " + username + " not found")
-                        );
-                return new UserPrincipal(user);
-            }
+        return username -> {
+            User user = userRepository.findByEmail(username)
+                    .orElseThrow(
+                            () -> new UsernameNotFoundException("User " + username + " not found")
+                    );
+            return new UserPrincipal(user);
         };
     }
 
