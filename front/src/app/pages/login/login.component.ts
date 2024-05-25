522a8067e7615621c79149ef04411b09934aeee9
Eric Heintzmann <heintzmann.eric@free.fr>
2024-05-25 14:18:41 +0200

    front: init top-bar shared component

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from "@angular/cdk/layout";
-import { map, Observable } from "rxjs";
 import { AsyncPipe, NgOptimizedImage } from "@angular/common";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
@@ -8,6 +6,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule, MatIconButton } from "@angular/material/button";
 import { MatGridListModule } from "@angular/material/grid-list";
+import { TopBarComponent } from "../../shared/top-bar/top-bar.component";
 
 @Component({
   selector: 'app-login',
@@ -22,31 +21,20 @@ import { MatGridListModule } from "@angular/material/grid-list";
     NgOptimizedImage,
     MatButtonModule,
     ReactiveFormsModule,
+    TopBarComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
 
-  isPhone$!: Observable<boolean>;
   loginForm!: FormGroup;
   passwordPattern: string = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{8,}$"
 
-  constructor(
-    private responsive: BreakpointObserver,
-    private formBuilder: FormBuilder
-  ) {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.isPhone$ = this.responsive.observe([
-      Breakpoints.XSmall
-    ]).pipe(
-      map((result: BreakpointState) => {
-        return result.matches;
-      })
-    )
-
     this.loginForm = this.formBuilder.group({
       login: [
         null, [Validators.required, Validators.maxLength(255)]
---
+++ front/src/app/shared/top-bar/top-bar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from "@angular/cdk/layout";
+import { map, Observable } from "rxjs";
+import { AsyncPipe, NgOptimizedImage } from "@angular/common";
+import { MatIconModule } from "@angular/material/icon";
+import { MatIconAnchor } from "@angular/material/button";
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MatIconModule,
+    NgOptimizedImage,
+    MatIconAnchor
+  ],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css'
+})
+export class TopBarComponent implements OnInit {
+
+  isPhone$!: Observable<boolean>;
+
+  constructor(private responsive: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.isPhone$ = this.responsive.observe([
+      Breakpoints.XSmall
+    ]).pipe(
+      map((result: BreakpointState) => {
+        return result.matches;
+      })
+    )
+  }
+}
