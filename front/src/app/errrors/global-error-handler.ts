a7abdeef84a2c2696713a7bc687b588f5e8d1982
Eric Heintzmann <heintzmann.eric@free.fr>
2024-07-07 09:29:46 +0200

    front: global error refactor

--- front/src/app/errrors/global-error-handler.ts
+++ front/src/app/errrors/global-error-handler.ts
@@ -1,37 +1,38 @@
 import { ErrorHandler, Injectable } from "@angular/core";
 import { HttpErrorResponse } from "@angular/common/http";
-import { MatSnackBar } from "@angular/material/snack-bar";
 import { TokenService } from "../services/token.service";
 import { DisplayErrorService } from "./display-error.service";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
   constructor(
-    private _snackBar: MatSnackBar,
     private _tokenService: TokenService,
     private _displayErrorService: DisplayErrorService,
+    private router: Router,
   ) {
   }
   handleError(wrappedError: any): void {
-    this._displayErrorService.show(
-      this._filterError(wrappedError.rejection ?? wrappedError),'Fermer');
+    this._filterError(wrappedError.rejection ?? wrappedError).then(
+      (msg: string) => this._displayErrorService.show(msg, 'Fermer')
+    );
   }
 
-  private _filterError(error: any): string {
+  private async _filterError(error: any): Promise<string> {
 
     // Navigator OffLIne
     if (!(navigator.onLine)) {
       return 'Le navigateur est hors-ligne'
     }
     if (error instanceof HttpErrorResponse) {
-      return this._filterHttpError(error);
+      return await this._filterHttpError(error);
     }
 
     return 'Erreur inconnue';
   }
 
-  private _filterHttpError(error: HttpErrorResponse): string {
+  private async _filterHttpError(error: HttpErrorResponse): Promise<string> {
 
     // Invalid Country ID
     if (error.status === 400) {
@@ -40,7 +41,8 @@ export class GlobalErrorHandler implements ErrorHandler {
 
     if (error.status === 401) {
       this._tokenService.removeToken();
-      return 'Vous êtes déconnecté(e)'
+      await this.router.navigate(['/login']);
+      return 'Vous êtes déconnecté(e)';
     }
 
     // Other Server Error
